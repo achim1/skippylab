* fixes a severe bug in average waveform, where the   waveform itself was altered.

pyosci/fit.py
@@ -11,6 +11,10 @@ from copy import deepcopy as copy
 import scipy.optimize as optimize
 import seaborn.apionly as sb
 import dashi as d
+
+from . import tools
+from . import plotting as plt
+
 from scipy.constants import elementary_charge as ELEMENTARY_CHARGE
 d.visual()
 
@@ -197,6 +201,7 @@ class Model(object):
         self.prediction = lambda xs: reduce(lambda x, y: x + y,\
                                   [f(xs) for f in self.components])
 
+
     def couple_models(self, coupling_variable):
         """
         Couple the models by a variable, which means use the variable not
@@ -225,6 +230,8 @@ class Model(object):
             None
         """
         self.all_coupled = True
+        # if self.all_coupled:
+        startparams = self.startparams[0:self.n_params[0]]
 
     def __add__(self, other):
         self._callbacks = self._callbacks + other._callbacks
@@ -330,8 +337,8 @@ class Model(object):
             return first
 
         startparams = self.startparams
-        if self.all_coupled:
-            startparams = self.startparams[0:self.n_params[0]]
+        #if self.all_coupled:
+        #    startparams = self.startparams[0:self.n_params[0]]
         bins = np.linspace(min(data), max(data), nbins)
         self.data = d.factory.hist1d(data, bins).normalized(density=True)
         self.xs = self.data.bincenters
@@ -425,7 +432,7 @@ class Model(object):
         return fig
 
 #mu_p, sigma_p, mu, sigma , lmbda
-def fit_model(data, nbins, model, start_params, **kwargs):
+def fit_model_deprecated(data, nbins, model, start_params, **kwargs):
     """
 
 
@@ -476,6 +483,43 @@ def fit_model(data, nbins, model, start_params, **kwargs):
     chi2 = (calculate_chi_square(h.bincontent, norm * model(h.bincenters, *parameters)))
     print("Obtained chi2 and chi2/ndf of {:4.2f} {:4.2f}".format(chi2, chi2 / nbins))
     return parameters
+
+
+def fit_model(waveformfile, model, startparams, \
+              rej_outliers=True, nbins=200, \
+              parameter_text=((r"$\mu_{{SPE}}$& {:4.2e}\\", 5),), **kwargs):
+    """
+    Standardazied fitting routine
+
+    Args:
+        waveformfile (str): full path to a file with waveforms saved by pyosci
+        model (pyosci.fit.Model): A model to fit to the data
+        startparams (tuple): initial parameters to model
+
+    Keyword Args:
+        rej_outliers (bool): Remove extreme outliers from data
+        nbins (int): Number of bins
+        parameter_text (tuple): will be passed to model.plot_result
+    Returns:
+        tuple
+    """
+    head, wf = tools.load_waveform(waveformfile)
+    plt.plot_waveform(head, tools.average_wf(wf))
+    charges = 1e12 * np.array([-1 * tools.integrate_wf(head, w) for w in wf])
+    # charges += np.ones(len(charges))
+    if rej_outliers:
+        charges = reject_outliers(charges)
+    model.startparams = startparams
+    model.fit_to_data(charges, nbins, **kwargs)
+    fig = model.plot_result(ymin=1e-4, \
+                            add_parameter_text=parameter_text, \
+                            xlabel=r"$Q$ [pC]")
+    if hasattr(model, "parameter_names"):
+        pretty_pars = [k for k in zip(model.parameter_names, model.best_fit_params)]
+    print("Best fit parameters {}".format(model.best_fit_params))
+    ax = fig.gca()
+    ax.grid(1)
+    return ax, model
 
 if __name__ == "__main__":
 

pyosci/osci.py
@@ -197,9 +197,12 @@ class TektronixDPO4104B(object):
         bincenters = r_binedges + (r_binedges - l_binedges)/2.
         return bincenters, bincontent 
 
-    def get_wf_header(self):
+    def get_wf_header(self, absolute_timing=False):
         """
         Get some meta information about the *next incoming wavefrm*
+
+        Keyword Args:
+            absolute_timing (bool): header["xs"] starts with header["xzero"], 0 otherwiese
 
         Returns:
             dict
@@ -208,10 +211,11 @@ class TektronixDPO4104B(object):
         header = cmd.parse_custom_wf_header(header)
         self.wf_buff_header = header
         #header["xs"] = np.ones(len(header["npoints"]))*header["xzero"]
-        xs = np.ones(header["npoints"])*header["xzero"]
-
-        # relative timing?
-        xs = np.zeros(int(header["npoints"]))
+        if absolute_timing:
+            xs = np.ones(header["npoints"])*header["xzero"]
+        else:
+            # relative timing?
+            xs = np.zeros(int(header["npoints"]))
         # FIXME: There must be a better way
         for i in range(int(header["npoints"])):
             xs[i] += i*header["xincr"]

pyosci/tools.py
@@ -6,7 +6,8 @@ Convenient operations
 import numpy as np
 import scipy.integrate as integrate
 from scipy.constants import elementary_charge as ECHARGE
-                #time.sleep(ACQTIME)
+from copy import deepcopy as copy
+
 IMPEDANCE = 50
 
 def average_wf(waveforms):
@@ -19,7 +20,7 @@ def average_wf(waveforms):
     Returns:
         np.ndarray
     """
-    wf0 = waveforms[0]
+    wf0 = copy(waveforms[0])
     for wf in waveforms[1:]:
         wf0 += wf
 