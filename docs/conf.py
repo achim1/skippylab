2216a52ed4352024f6cd83a926f4201068dbf927
Achim Stoessl <stoessl@hawaii.edu>
2018-05-09 03:34:13 -1000

    * attempt to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -63,8 +63,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon']#,
-    ##'sphinx.ext.githubpages']
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
